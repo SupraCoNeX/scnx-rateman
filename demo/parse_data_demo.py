62035d00130805481e8f54e8b6a2eb0cc6b9074f
Sankalp <stellar.sankalp@gmail.com>
2021-06-17 16:25:35 +0200

    clean demo folder to have single demo for parsing data

--- demo/parse_data_demo.py
+++ demo/parse_data_demo.py
@@ -10,13 +10,11 @@ Demo for parsing data (TXS and RCSTATS) into a .csv file per AP
 This demo script illustrates the basic use of the RateManager package to 
 parse data from multiple APs into designated .csv files. Data denotes TX Status
 Rate Control Statistics piped using the 'phyX;start;stats;txs' command with 
-minstrel-rcd. In the current version, rate setting is done with random rate 
-indices. In the next steps, an certain algorithm will be used to derive the 
-rate to set.
+minstrel-rcd. 
 
 Run the demo script in the terminal using,
 
-python parse_data_set_rate_demo.py
+python parse_data_demo.py
 
 
 RateMan is stopped within the terminal, observe the print statements.
@@ -28,13 +26,22 @@ import rateman
 import time
 import paramiko
 
-
 if __name__ == "__main__":
 
     # # Create rateman object
-  
+    
+    ### pre-requisites
+    
+        
+    duration = 10 
+    
     rateMan = rateman.RateMan()
 
     rateMan.addaccesspoints("sample_ap_lists/ap_list_sample_1.csv")
 
-    rateMan.start()
+    rateMan.start(duration)
+    
+    
+    ### clean-up
+   
+ 
