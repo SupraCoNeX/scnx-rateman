22a03d06c0994cf62ae7cace2257aa64c6372489
Martin Le <le@ifn.ing.tu-bs.de>
2021-07-15 19:02:21 +0200

    utils.py: Add flag error lines in tx-stats-traces data

--- RateMan/rateman/utils.py
+++ RateMan/rateman/utils.py
@@ -4,10 +4,12 @@ import signal
 
 import pandas as pd
 import numpy as np
+from pandas.io.parsers import read_csv
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # TODO: Output of tx status and stats are not consistent. Sometimes already converted.
+#TODO: Improve Timestamp 2 interpolation. Very dirty now. 
 
 
 __all__ = ["read_stats_txs_csv", "timedInput"]
@@ -32,19 +34,22 @@ def read_stats_txs_csv(
     """
 
     if bin_enc:
-        p = io.StringIO(data.decode("utf-8"))
+        p = io.StringIO(data.decode('utf-8'))
     else:
         p = Path(data)
         if not p.exists():
             raise FileNotFoundError
     # Read CSV file containing tx status and rc_stats and save in
     # dataframe `df`.
-    df = pd.read_csv(p, sep=";", header=3)
+    df = pd.read_csv(p, sep=";", names=range(13))
+    df['timestamp_error'] = False
+    df['parsing_error'] = False
     # Read tx status from dataframe `df`.
-    txs_data = df[df.iloc[:, 2] == "txs"].iloc[:, :9]
+    txs_data = df[df.iloc[:, 3] == "txs"]
     txs_data.columns = [
         "phy_nr",
         "timestamp",
+        "timestamp2",
         "type",
         "macaddr",
         "num_frames",
@@ -52,12 +57,15 @@ def read_stats_txs_csv(
         "probe",
         "rates",
         "counts",
+        "parsing_error",
+        "timestamp_error",
     ]
     # Read rc_stats from dataframe `df`.
-    stats_data = df[df.iloc[:, 2] == "stats"]
+    stats_data = df[df.iloc[:, 3] == "stats"]
     stats_data.columns = [
         "phy_nr",
         "timestamp",
+        "timestamp2"
         "type",
         "macaddr",
         "rate",
@@ -67,52 +75,154 @@ def read_stats_txs_csv(
         "cur_attempts",
         "hist_success",
         "hist_attempts",
+        "parsing_error",
+        "timestamp_error",
     ]
     # stats_data_idx = stats_data.index
     # txs_data_idx = txs_data.index
     # rest_data_idx = df.index.difference(stats_data_idx.union(txs_data_idx))
     # rest_data = df[rest_data_idx]
-    if humanread:
-        # Convert boot timestamp into seconds (uptime of system)
-        to_sec = lambda x: int(x, 16) / 1000000000
-        txs_data["timestamp"] = txs_data.timestamp.apply(to_sec)
-        stats_data["timestamp"] = stats_data.timestamp.apply(to_sec)
-        # Convert avg throughput from hex to bit/s
-        to_bits = lambda x: int(x, 16)
-        try:
-            stats_data["avg_tp"] = stats_data.avg_tp.apply(to_bits)
-        except TypeError:
-            print("Average Throughput probably already converted...")
     # Reset index of dataframes
     txs_data = txs_data.reset_index(drop=True)
     stats_data = stats_data.reset_index(drop=True)
-    # Omit probably defective packets (wrong timestamp) and shift time to
-    # zero if `shifttime = True`.
+    #TODO: Check: Omit probably defective packets (wrong timestamp)
     if txs_data.empty:
         txs_ts0 = 0
     else:
         txs_ts0 = txs_data.loc[0].timestamp
+        txs_data = txs_data[txs_data["timestamp"] > txs_ts0]
     if stats_data.empty:
         stats_ts0 = 0
     else:
         stats_ts0 = stats_data.loc[0].timestamp
-    min_time = np.amin([txs_ts0, stats_ts0])
-    if not txs_data.empty:
-        txs_data = txs_data[txs_data["timestamp"] > txs_ts0]
-        if shifttime:
-            shifttxs = lambda x: x - min_time
-            txs_data["timestamp"] = txs_data.timestamp.apply(shifttxs)
-    if not stats_data.empty:
         stats_data = stats_data[stats_data["timestamp"] > stats_ts0]
-        if shifttime:
-            shiftstats = lambda x: x - min_time
-            stats_data["timestamp"] = stats_data.timestamp.apply(shiftstats)
     # Set timestamps as index for both dataframes `txs_data` and
     # `stats_data`.
     txs_data = txs_data.set_index("timestamp")
     stats_data = stats_data.set_index("timestamp")
     return txs_data, stats_data
 
+def flag_error_in_data(file):
+    """Flag error lines in traces. There are timestamp errors and parsing errors
+    if they are not timestamp errors.
+    """
+    df = read_csv(file, sep=';', names=range(14))
+    df.columns = [
+        "phy_nr",
+        "timestamp",
+        "timestamp2",
+        "type",
+        "macaddr",
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        "parsing_error",
+        "timestamp_error",
+    ]
+    # Skip ap_info lines
+    ap_info_len = df.loc[df['timestamp2'] == 'group'].index[-1]
+    phy_len = df['phy_nr'].unique().size
+    df_data = df.iloc[ap_info_len+phy_len:]
+    # Set the `parsing_error` flag of lines with NaN as type to True
+    df_data.loc[:, 'parsing_error'] = False
+    df_data.loc[:, 'timestamp_error'] = False
+    df_data.loc[df_data.type.isna(), 'parsing_error'] = True
+    ts_new, ts_error = _interpolate_error_timestamps(df_data.timestamp)
+    df_data.loc[:, 'timestamp'] = ts_new
+    df_data.loc[ts_error, 'timestamp_error'] = True
+    ts_new2, ts_error2 = _interpolate_error_timestamps(df_data.timestamp2, True)
+    df_data.loc[:, 'timestamp2'] = ts_new2
+    df_data.loc[ts_error2, 'timestamp_error'] = True
+    return df_data
+
+def _interpolate_error_timestamps(timestamps, ns=False, error_interval=1000):
+    """Interpolate error timestamp.
+    """
+    ts = []
+    ts_errors = []
+    first_entry = True
+    running_flag = False
+    last_valid = 0
+    counter = 0
+    for i, t in enumerate(timestamps):
+        # Special case: 1st entry
+        # Wait until receiving a valid timestamp but could be not correct
+        # Therefore, there is the `running_flag` which should detect the wrong
+        # first timestamp
+        if first_entry:
+            if pd.notna(t) and t.isnumeric():
+                ts.append(int(t))
+                for c in range(1, counter+1):
+                    ts[i-c] = t-c
+                first_entry = False
+                last_valid = i
+                counter = 0
+                ts_errors.append(False)
+            else:
+                ts.append(pd.NA)
+                counter += 1
+                ts_errors.append(True)
+        elif not running_flag and pd.notna(t) and t.isnumeric():
+            diff = int(t) - ts[last_valid]
+            if ns:
+                ts.append(int(t))
+                for j in range(1, counter+1):
+                    ts[i-j] = int(t)-j
+                counter = 0
+                last_valid = i
+                ts_errors.append(False)
+            else:
+                if diff < 0:
+                    ts.append(pd.NA)
+                    counter += 1
+                    ts_errors.append(True)
+                elif diff > error_interval:
+                    ts.append(int(t))
+                    for j in range(1, counter+1):
+                        ts[i-j] = int(t)-j
+                    running_flag = True
+                    last_valid = i
+                    counter = 0
+                    ts_errors.append(False)
+                elif 0 < diff <= error_interval:
+                    ts.append(int(t))
+                    step_width = diff/(counter+1)
+                    for j in range(1, counter+1):
+                        ts[i-j] = int(int(t) - j * step_width)
+                    running_flag = True
+                    last_valid = i
+                    counter = 0
+                    ts_errors.append(False)
+        elif pd.notna(t) and t.isnumeric():
+            if ns:
+                ts.append(int(t))
+                step_width = (int(t) - ts[last_valid]) / (counter + 1)
+                for j in range(1, counter+1):
+                    ts[i-j] = int(int(t) - j * step_width)
+                counter = 0
+                last_valid = i
+                ts_errors.append(False)
+            elif np.abs(int(t) - ts[last_valid]) <= error_interval and not ns:
+                ts.append(int(t))
+                step_width = (int(t) - ts[last_valid]) / (counter + 1)
+                for j in range(1, counter+1):
+                    ts[i-j] = int(int(t) - j * step_width)
+                last_valid = i
+                counter = 0
+                ts_errors.append(False)
+            else:
+                ts.append(pd.NA)
+                counter += 1
+                ts_errors.append(True)
+        else:
+            ts.append(pd.NA)
+            counter += 1
+            ts_errors.append(True)
+    return ts, ts_errors
 
 def timedInput(prompt="", timeout=1, timeoutmsg=None):
     def timeout_error(*_):
@@ -129,9 +239,3 @@ def timedInput(prompt="", timeout=1, timeoutmsg=None):
             print(timeoutmsg)
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
         return None
-
-
-# if __name__ == '__main__':
-#     csvfile = 'demo/collected_data/data_AP1.csv'
-#     txs, stats = read_stats_txs_csv(csvfile, True)
-#     print("Done")
