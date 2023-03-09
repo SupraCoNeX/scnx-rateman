4fd0b4d48e1c236f76de95c48be9828ac95cc6e5
Sankalp Pawar <sankalp-prakash.pawar@hs-nordhausen.de>
2023-03-09 17:20:13 +0100

    used dump_stas() and reset_stats(radio, sta)

--- rateman/rateman.py
+++ rateman/rateman.py
@@ -127,7 +127,7 @@ class RateMan:
         ap.set_rc_info(False)
         for radio in ap.radios:
             ap.apply_system_config(radio)
-            ap.send(f"{radio};dump")
+            ap.dump_stas(radio)
             ap.set_rc_info(True, radio)
 
         self.add_task(
