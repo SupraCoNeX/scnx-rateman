d4bf55a6756f839ec08a0cf1f53a82bff35304da
Arne Kappen <arne.kappen@hhi.fraunhofer.de>
2023-06-12 15:44:35 +0200

    __init__.py: tabs to spaces
    Signed-off-by: Arne Kappen <arne.kappen@hhi.fraunhofer.de>

--- rateman/__init__.py
+++ rateman/__init__.py
@@ -15,21 +15,20 @@ Motivation
 
 """
 
-
 __version__ = "0.1"
 
 import sys
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 11:
-	print(
-	    "SupraCoNeX Experimentor requires Python 3.11! "
-	    f"You are running {sys.version_info[0]}.{sys.version_info[1]}",
-	    file=sys.stderr
-	)
-	sys.exit(1)
+    print(
+        "SupraCoNeX Experimentor requires Python 3.11! "
+        f"You are running {sys.version_info[0]}.{sys.version_info[1]}",
+        file=sys.stderr
+    )
+    sys.exit(1)
 
-from .rateman import *
 from .accesspoint import *
 from .station import *
+from .rateman import *
 from .parsing import *
 from .rate_control import *
