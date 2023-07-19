2e1e11d87b39d3fba937255630c3b87545f709c1
Arne Kappen <arne.kappen@hhi.fraunhofer.de>
2023-07-19 13:15:33 +0200

    examples: update basic example
    Signed-off-by: Arne Kappen <arne.kappen@hhi.fraunhofer.de>

--- examples/basic.py
+++ examples/basic.py
@@ -6,15 +6,16 @@
 import asyncio
 import sys
 import rateman
-from common import setup_argparser
+from common import parse_arguments, setup_logger
+
 
 if __name__ == "__main__":
-    arg_parser = setup_argparser()
-    args = arg_parser.parse_args()
-    aps = rateman.from_strings(args.accesspoints)
+    log = setup_logger("basic")
+    args = parse_arguments()
+    aps = rateman.from_strings(args.accesspoints, logger=log)
 
     if args.ap_file:
-        aps += rateman.from_file(args.ap_file)
+        aps += rateman.from_file(args.ap_file, logger=log)
 
     if len(aps) == 0:
         print("ERROR: No accesspoints given", file=sys.stderr)
@@ -23,18 +24,31 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    print("Running rateman...")
-    rateman_obj = rateman.RateMan(aps, loop=loop)
+    rm = rateman.RateMan(loop=loop, logger=log)
 
-    # add a simple print callback to see the raw incoming data
-    rateman_obj.add_raw_data_callback(
-        lambda ap, fields: print(f"{ap.name} > '{fields}'")
-    )
+    # set AP's logger and add AP to rateman
+    for ap in aps:
+        rm.add_accesspoint(ap)
+
+    # establish connections and set up state
+    loop.run_until_complete(rm.initialize())
+
+    # Enable only 'txs' and 'stats' events
+    for ap in aps:
+        loop.run_until_complete(ap.disable_events())
+        loop.run_until_complete(ap.enable_events(["txs", "stats"]))
+
+    # add a simple print callback to see the incoming data
+    def print_event(ap, ev, context=None):
+        print(f"{ap.name} > {ev}")
+
+    rm.add_raw_data_callback(print_event)
 
     try:
+        print("Running rateman...")
         loop.run_forever()
-    except (OSError, KeyboardInterrupt):
+    except KeyboardInterrupt:
         print("Stopping...")
     finally:
-        loop.run_until_complete(rateman_obj.stop())
+        loop.run_until_complete(rm.stop())
         print("DONE")
--- examples/common.py
+++ examples/common.py
@@ -1,13 +1,14 @@
 import argparse
 import sys
+import logging
 
 from rateman.accesspoint import AccessPoint
 
 
-def setup_argparser():
+def parse_arguments():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
-        "-rc-alg",
+        "-g", "--rc-algorithm",
         metavar="RC_ALG",
         type=str,
         choices=["minstrel_ht_kernel_space", "py_minstrel_ht", "manual_mrr_setter"],
@@ -16,19 +17,19 @@ def setup_argparser():
     )
 
     arg_parser.add_argument(
-        "-rc-opts",
+        "-o", "--rc-options",
         metavar="RC_OPTS",
-        default="{}",
+        type=dict,
+        default={},
         help="Rate control options",
     )
 
     arg_parser.add_argument(
-        "-A",
-        "--ap-file",
+        "-A", "--ap-file",
         metavar="AP_FILE",
         type=str,
         help="Path to a csv file where each line contains information about an access point "
-        + "in the format: ID,ADDR,RCDPORT.",
+        + "in the format: NAME,ADDR,RCDPORT",
     )
     arg_parser.add_argument(
         "accesspoints",
@@ -39,4 +40,12 @@ def setup_argparser():
         + "RCDPORT is optional and defaults to 21059.",
     )
 
-    return arg_parser
+    return arg_parser.parse_args()
+
+
+def setup_logger(name, log_lvl=logging.DEBUG):
+    logging.basicConfig(format="[LOG] %(asctime)s - %(name)s - %(message)s")
+    logger = logging.getLogger(name)
+    logger.setLevel(log_lvl)
+
+    return logger
