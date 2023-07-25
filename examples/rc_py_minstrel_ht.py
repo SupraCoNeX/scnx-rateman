ae51774b54ae85f5d747aee8f9325d466bb5a6e7
Arne Kappen <arne.kappen@hhi.fraunhofer.de>
2023-07-25 16:36:58 +0200

    add manual mrr setter and py-minstrel examples
    Signed-off-by: Arne Kappen <arne.kappen@hhi.fraunhofer.de>

---
+++ examples/rc_py_minstrel_ht.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF8 -*-
+# Copyright SupraCoNeX
+#     https://www.supraconex.org
+#
+
+import asyncio
+import sys
+import rateman
+import time
+
+from common import parse_arguments, setup_logger
+
+
+if __name__ == "__main__":
+    log = setup_logger("py_minstrel_ht")
+    args = parse_arguments()
+    aps = rateman.from_strings(args.accesspoints, logger=log)
+
+    if args.ap_file:
+        aps += rateman.from_file(args.ap_file, logger=log)
+
+    if len(aps) == 0:
+        print("ERROR: No accesspoints given", file=sys.stderr)
+        sys.exit(1)
+
+    # create asyncio event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # create rateman object
+    rm = rateman.RateMan(loop=loop, logger=log)
+
+    # add APs to rateman
+    for ap in aps:
+        rm.add_accesspoint(ap)
+
+    # establish connections and set up state
+    loop.run_until_complete(rm.initialize())
+
+    # start 'manual_mrr_setter' user space rate control algorithm.
+    for ap in aps:
+        for sta in ap.get_stations():
+            loop.run_until_complete(
+                sta.start_rate_control(
+                    "py_minstrel_ht",
+                    {
+                        "filter": "Butterworth",
+                        "reset_rate_stats": True,
+                        "kern_sample_table": True
+                    }
+                )
+            )
+
+    # Enable 'txs' events so we can see our rate setting in action. Note, this requires traffic to
+    # produce events. pinging the station across the wireless link can help with that.
+    for ap in aps:
+        loop.run_until_complete(ap.disable_events())
+        loop.run_until_complete(ap.enable_events(["txs"]))
+
+    # add a simple print callback to see the txs events
+    def print_event(ap, ev, context=None):
+        print(f"{ap.name} > {ev}")
+
+    rm.add_raw_data_callback(print_event)
+
+    try:
+        print("Running rateman... (Press CTRL+C to stop)")
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Stopping...")
+    finally:
+        loop.run_until_complete(rm.stop())
+        print("DONE")
