d14e9e70ee4e5bfcaaafed7d799c41405c922b64
Sankalp Pawar <sankalp-prakash.pawar@hs-nordhausen.de>
2024-05-06 13:49:00 +0200

    rate_idx as hex for get_rate_info()

--- rateman/accesspoint.py
+++ rateman/accesspoint.py
@@ -170,7 +170,7 @@ class AccessPoint:
     def sample_table(self, sample_table_data):
         self._sample_table = [list(map(int, row.split(","))) for row in sample_table_data]
 
-    def get_rate_info(self, rate_idx: int) -> dict:
+    def get_rate_info(self, rate_idx) -> dict:
         rate_info = get_rate_info(self._all_rate_info, rate_idx)
 
         return rate_info
