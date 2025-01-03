697570de98a1b65d23314a369c2f937185fe1731
Raul Shahi <rahulfromnepal2013@gmail.com>
2025-01-03 11:25:39 +0100

    comments modified

--- rateman/station.py
+++ rateman/station.py
@@ -452,7 +452,7 @@ class Station:
 
     async def orca_log(self, log_string: str):
        """
-       Asynchronously logs a message to the ORCA (Open Radio Communication Architecture) interface of the associated access point.
+       Asynchronously logs a message to the ORCA interface of the associated access point.
        """
        await self._accesspoint.orca_log(self.radio, f"{self._mac_addr};{log_string}")
 
